28ff93a298432b71a02cda0b39465a792a9a5ff9
cangnss <cgunes52@gmail.com>
2022-12-19 23:06:06 +0300

    added navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import logo from "../images/repaircar.png"
+
+export default function Navbar(){
+    return(
+        <div className="w-full flex flex-row justify-around items-center">
+            <div>
+                <img src={logo} alt="repair" width="150" />
+            </div>
+            <div className="flex flex-row">
+                <ul className="flex flex-row">
+                    <li className="font-semibold mr-2 hover:text-gray-50">Home</li>
+                    <li className="font-semibold mr-2 hover:text-gray-50">About</li>
+                </ul>
+            </div>
+            <div className="flex flex-row mt-1">
+                <div className="w-full">
+                    <button className="p-1 w-24 mr-2 font-semibold text-white bg-blue-600 border-2 border-blue-600 rounded-lg">Login</button>
+                </div>
+                <div className="">
+                    <button className="p-1 w-24 font-semibold text-black bg-white rounded-lg border-2 border-blue-600">Register</button>
+                </div>
+            </div>
+        </div>
+    );
+}
