4210a502647b2942516d5087577ccde4659c13ac
cangnss <cgunes52@gmail.com>
2022-12-20 13:55:25 +0300

    added register page and designed login page

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,30 +1,36 @@
 export default function Login() {
   return (
-    <div className="w-72 p-1 mt-10 border-2 rounded-lg shadow-lg flex flex-col justify-center border-2 border-blue-600">
-      <div className="flex flex-row mb-2">
-        <div className="p-1">
-          <label> Email</label>
+    <div className="w-full mt-10">
+      <div className="mx-auto w-96 p-10 border-2 rounded-lg shadow-lg flex flex-col justify-center border-blue-600">
+        <div className="flex flex-row">
+          <div className="mt-1 flex flex-col">
+            <div className="mb-10">
+              <label className="font-semibold">Email</label>
+            </div>
+            <div className="mb-3 mr-2">
+              <label className="font-semibold">Password</label>
+            </div>
+          </div>
+          <div>
+            <div className="mb-5 mr-5">
+              <input
+                type="email"
+                className="w-48 h-10 border-2 border-slate-200 rounded-lg"
+              />
+            </div>
+            <div className="mb-5 mr-5">
+              <input
+                type="password"
+                className="w-48 h-10 border-2 border-slate-200 rounded-lg"
+              />
+            </div>
+          </div>
         </div>
-        <div>
-          <input
-            type="email"
-            className="w- h-10 border-2 border-slate-200 rounded-lg"
-          />
+        <div className="flex justify-end">
+          <button className="w-32 p-1 rounded-lg border-2 border-blue-600 bg-blue-600 text-white font-semibold">
+            Login
+          </button>
         </div>
-      </div>
-      <div className="flex flex-row">
-        <div className="p-1">
-          <label>Password</label>
-        </div>
-        <div>
-          <input
-            type="password"
-            className="w- h-10 border-2 border-slate-200 rounded-lg"
-          />
-        </div>
-      </div>
-      <div>
-        <button className="w-32 rounded-lg border-2 border-blue-600 bg-blue-600 text-white font-semibold">Login</button>
       </div>
     </div>
   );
--- src/components/Auth/Signup.js
+++ src/components/Auth/Signup.js
@@ -1,7 +1,57 @@
-export default function Signup(){
-    return(
-        <div>
-            signupasdsadas
+export default function Signup() {
+  return (
+    <div className="p-5 w-96 mx-auto mt-10 shadow-lg border-2 border-blue-600 rounded-lg">
+      <div className="flex flex-row">
+        <div className="mt-1 flex flex-col">
+          <div className="mb-5 mr-2">
+            <label className="font-semibold">Firstname: </label>
+          </div>
+          <div className="mt-5">
+            <label className="font-semibold">Lastname: </label>
+          </div>
+          <div className="mt-10">
+            <label className="font-semibold">Email: </label>
+          </div>
+          <div className="mt-8">
+            <label className="font-semibold">Password: </label>
+          </div>
         </div>
-    )
+        <div className="flex flex-col">
+          <div className="mb-5">
+            <input
+              type="text"
+              className="w-48 h-10 border-2 border-slate-200 rounded-lg"
+              name="firstname"
+            />
+          </div>
+          <div className="mb-5">
+            <input
+              type="text"
+              className="w-48 h-10 border-2 border-slate-200 rounded-lg"
+              name="lastname"
+            />
+          </div>
+          <div className="mb-5">
+            <input
+              type="email"
+              className="w-48 h-10 border-2 border-slate-200 rounded-lg"
+              name="email"
+            />
+          </div>
+          <div className="mb-5">
+            <input
+              type="password"
+              className="w-48 h-10 border-2 border-slate-200 rounded-lg"
+              name="password"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-end">
+        <button className="w-32 p-1 rounded-lg border-2 border-blue-600 bg-blue-600 text-white font-semibold">
+          Signup
+        </button>
+      </div>
+    </div>
+  );
 }
