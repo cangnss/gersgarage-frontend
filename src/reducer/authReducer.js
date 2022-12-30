dbc55d1f28f40e13de0788b84bf59662b5a6597c
cangnss <cgunes52@gmail.com>
2022-12-30 07:59:18 +0300

    added user's context and auth

---
+++ src/reducer/authReducer.js
@@ -0,0 +1,22 @@
+export default function reducer(state, action) {
+  switch (action.type) {
+    case "LOGIN_SUCCESS":
+      console.log("Action payload:", action.payload);
+      localStorage.setItem("user", JSON.stringify(action.payload.user));
+      localStorage.setItem("token", JSON.stringify(action.payload.token));
+      return {
+        ...state,
+        user: action.payload.user,
+        token: action.payload.token,
+      };
+    case "LOGOUT":
+      localStorage.removeItem("user");
+      return {
+        ...state,
+        user: null,
+        token: null,
+      };
+    default:
+      return state;
+  }
+}
