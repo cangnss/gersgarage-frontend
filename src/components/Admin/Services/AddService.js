be3b8c6e5e1ec50e796059e3fd1d292844b5c3b9
cangnss <cgunes52@gmail.com>
2023-01-13 14:13:49 +0300

    added new service notification

--- src/components/Admin/Services/AddService.js
+++ src/components/Admin/Services/AddService.js
@@ -1,29 +1,66 @@
 import { useState } from "react";
 import { postPlaceServiceData } from "../../../service/api";
+import Success from "../../Success";
+import Error from "../../Error";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 
 export default function AddService() {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({});
-
+  const [notify, setNotify] = useState({
+    success: false,
+    error: false,
+  });
   const handleChange = (event) => {
     const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
+    setFormData({ ...formData, [name]: value, placeId: 1 });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     postPlaceServiceData(formData)
       .then((response) => {
-        console.log(response.data);
+        console.log(response);
+        if (response.status === 201) {
+          setNotify({
+            success: true,
+            error: false,
+            message: "Added New Service!",
+          });
+          setTimeout(() => {
+            setNotify({ success: false });
+            navigate('/admin/services');
+          }, 2000);
+        }
       })
       .catch((error) => {
-        console.error(error);
+        console.error("err", error);
+        if (error) {
+          setNotify({
+            success: false,
+            error: true,
+            message: "Something went wrong!"
+          });
+          setTimeout(() => {
+            setNotify({ error: false });
+          }, 3000);
+        }
       });
   };
   console.log("formdata", formData);
 
   return (
     <div className="w-full mt-10">
+      {notify.success ? <Success message={notify.message} /> : null}
+      {notify.error ? <Error message={notify.message} /> : null}
       <div className="mx-auto w-96 p-10 border-2 rounded-lg shadow-lg flex flex-col justify-center border-blue-600">
+        <AiOutlineArrowLeft
+          className="text-2xl"
+          onClick={() => {
+            navigate(-1);
+          }}
+        />
         <form onSubmit={handleSubmit}>
           <div className="flex flex-row">
             <div className="mt-1 flex flex-col">
@@ -52,12 +89,14 @@ export default function AddService() {
                   defaultValue={formData?.price || ""}
                   onChange={handleChange}
                 />
-                <input type="hidden" name="placeId" defaultValue={formData?.placeId || 1} value="1" />
               </div>
             </div>
           </div>
           <div className="flex justify-end">
-            <button type="submit" className="w-32 p-1 rounded-lg border-2 border-blue-600 bg-blue-600 text-white font-semibold">
+            <button
+              type="submit"
+              className="w-32 p-1 rounded-lg border-2 border-blue-600 bg-blue-600 text-white font-semibold"
+            >
               Add Service
             </button>
           </div>
